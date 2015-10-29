4b32e2da13c927d862552efc67241cdcab56ee1a
TeemuKoivisto <teemu.koivisto@helsinki.fi>
2015-10-29 15:32:54 +0200

    Add Posts-view

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,34 +6,15 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
-
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1446117196596;
-    vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
+  function MainController($scope) {
+    $scope.posts = [
+      {
+        title: 'Näin Venäjä hyötyy Suomen pakolaiskriisistä – Tutkija: Kremlissä mietitty, voidaanko kaataa bensaa liekkeihin',
+        url: 'http://www.iltasanomat.fi/kotimaa/art-1446092580173.html'
+      },
+      {
+        title: 'Ihmiset levittävät tietämättään virusta – klikkasitko tätä linkkiä Facebookissa?',
+	url: 'http://www.iltasanomat.fi/digi/art-1446091738032.html'
+      }
   }
 })();
